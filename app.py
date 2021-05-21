e3229883a6475dcd1224ede26048b0cde286cac0
JorisPaarde <jorispaarde@gmail.com>
2021-05-21 12:45:01 +0000

    Added database add functionality

--- app.py
+++ app.py
@@ -25,6 +25,49 @@ def all_recipes():
     return render_template("all_recipes.html", recipes=recipes)
 
 
+@app.route("/add_recipe", methods=["GET", "POST"])
+def add_recipe():
+    if request.method == "POST":
+
+        ingredient_1 = {
+            "unit_name": "yet to add",
+            "amount": request.form.get("amount"),
+            "ingredient_name": request.form.get("amount")
+        }
+
+        ingredient_2 = {
+            "unit_name": "yet to add also",
+            "amount": request.form.get("amount"),
+            "ingredient_name": request.form.get("amount")
+        }
+
+        ingredients = [ingredient_1,ingredient_2]
+
+        step_1 = request.form.get("preparation_step")
+        step_2 = "this is step 2"
+
+        preparation_steps = [step_1, step_2]
+
+        liked_by = ["joris", "ben", "henk"]
+
+        recipe = {
+            "user_name": "yet to add",
+            "recipe_title": request.form.get("recipe_title"),
+            "recipe_description": request.form.get("recipe_description"),
+            "image_url": request.form.get("image_url"),
+            "category_name": "yet to add",
+            "ingredients": ingredients,
+            "preparation_steps": preparation_steps,
+            "liked_by": liked_by
+        }
+
+        mongo.db.recipes.insert_one(recipe)
+        flash("Task succesfully added")
+        return redirect(url_for("all_recipes"))
+
+    return render_template("add_recipe.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
