948c6b49b63d705a498947b9f3860de122a1dce3
JorisPaarde <jorispaarde@gmail.com>
2021-05-21 19:06:44 +0000

    Added fadeout on flashes

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,9 @@
   $(document).ready(function(){
     $('.sidenav').sidenav();
     $('select').formSelect();
+    fadeOutFlash();
   });
+
+  function fadeOutFlash(){
+    $('.flashes').delay(4000).slideUp('slow');
+  }
